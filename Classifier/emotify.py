ea1409a5ff0328126c988cd67fb33b724c70c33d
scholtes <scholtgt@mail.uc.edu>
2017-02-09 17:14:13 -0500

    Parse arff files for bextract data

--- Classifier/emotify.py
+++ Classifier/emotify.py
@@ -1,9 +1,10 @@
 import sys
 import os
 import glob
+import json
 from src import bextract
 
 files = [glob.glob(arg, recursive=True) for arg in sys.argv[1:]]
 files = [os.path.abspath(val) for subfiles in files for val in subfiles]
 
-bextract.extract(files)
+data = bextract.extract(files)
--- Classifier/src/bextract.py
+++ Classifier/src/bextract.py
@@ -1,6 +1,7 @@
 import os
-import sys
+import re
 import subprocess
+import sys
 
 BEXTRACT_DIRECTORY = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'bin'))
 BEXTRACT_FILENAME = os.path.join(BEXTRACT_DIRECTORY, "bextract.exe")
@@ -20,7 +21,10 @@ def extract(filenames):
 	returns:
 		A list of dicts containing bextract data.  Each element of list is formatted like: 
 		    {"name": "/full/path/to/music.mp3",
-		    "data": [0.1234, 0.4321, 0.420, ... ]}
+		    "data": [
+		    	[0.1234, 0.4321, 0.420, ... ],
+		    	[0.2345, 0.5432, 0.421, ... ], ...
+		    ]}
 	'''
 	files = [os.path.abspath(file) for file in filenames]
 	if not os.path.exists(TEMP_DIRECTORY):
@@ -37,12 +41,17 @@ def extract(filenames):
 		p.wait()
 	# Create mkcollection
 	tempfilesstrlist= "\n".join(tempfiles)
-	with open(MKCOLLECTION, 'w') as f:
+	with open(MKCOLLECTION, 'wb') as f:
 		f.write(tempfilesstrlist)
 	# Run bextract
 	p = subprocess.Popen([BEXTRACT_FILENAME, '-fe', '-ws', WINDOW_FS, '-hp', WINDOW_FS, '-od', TEMP_DIRECTORY+os.sep, MKCOLLECTION], cwd=TEMP_DIRECTORY)
 	p.wait()
-	# Cleanup 
+	# Parse bextract data
+	arff = ""
+	with open(os.path.join(TEMP_DIRECTORY, 'MARSYAS_EMPTY'), 'r') as f:
+		arff = f.read()
+	data = _parse_arff(arff, tempfiles, files)
+	# Cleanup
 	for file in tempfiles:
 		if os.path.isfile(file):
 			os.remove(file)
@@ -56,3 +65,26 @@ def extract(filenames):
 		os.rmdir(TEMP_DIRECTORY)
 	except OSError as ex:
 		pass
+	return data
+
+def _parse_arff(arff, tempfiles, properfiles):
+	'''
+	Takes: arff string from bextract
+	Returns: list that contains dictionaries that look like this:
+		{"name": "/full/path/to/music.mp3",
+	    "data": [
+	    	[0.1234, 0.4321, 0.420, ... ],
+	    	[0.2345, 0.5432, 0.421, ... ], ...
+	    ]}
+	'''
+	zippedfiles = dict(zip(tempfiles, properfiles))
+	dictions = []
+	nocrud = arff.split("@data")[1].strip()
+	blocks = [block.strip() for block in nocrud.split("% filename") if block.strip() != ""]
+	for block in blocks:
+		lines = block.split("\n")
+		name = lines[0]
+		datalines = lines[2:]
+		data = [[float(datum) for datum in dataline.split(",")[:-1]] for dataline in datalines]
+		dictions.append({"name": zippedfiles[name], "data": data})
+	return dictions
