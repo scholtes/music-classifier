23d150216658f2a3631375cc04460c2e3a909a01
scholtes <scholtgt@mail.uc.edu>
2017-02-09 17:57:25 -0500

    Added regression.  Prints json

--- Classifier/emotify.py
+++ Classifier/emotify.py
@@ -3,8 +3,11 @@ import os
 import glob
 import json
 from src import bextract
+from src import linregress
 
 files = [glob.glob(arg, recursive=True) for arg in sys.argv[1:]]
 files = [os.path.abspath(val) for subfiles in files for val in subfiles]
 
 data = bextract.extract(files)
+results = linregress.classify(data)
+print(json.dumps(results))
