e85278c68424fd39a1064f4d077bc5fa7c63aa02
Ricky Rossi <rossird@mail.uc.edu>
2017-02-22 13:09:52 -0500

    Adding classifer test project

---
+++ EmotionalGUI/ClassifierTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classifier;
+
+namespace ClassifierTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Classifier.SupportVectorMachine svm = new Classifier.SupportVectorMachine();
+            svm.Classify(new[] { @"T:\Documents\music-classifier\Classifier\test.mp3" });
+        }
+    }
+}
