3e0b12a1872a3735875bd06ed7c853c4110d47af
scholtes <scholtgt@mail.uc.edu>
2017-02-25 16:24:16 -0500

    Improve linear regression by searching for the optimal subset

--- training-set/garrett_testing/linear_regression.m
+++ training-set/garrett_testing/linear_regression.m
@@ -4,7 +4,8 @@
 data = xlsread('..\ratings\song_database\annotations_with_features.xlsx');
 
 % Which subset of attributes to use? 
-bextract = 4:12;
+bextract_pos = [4 5 11 12 13 16 20 25 29 30];
+bextract_int = [4 6 7 8 9 11 13 17 19 22 23 26 29 32];
 
 % Seed 
 rng(2017);
@@ -16,12 +17,14 @@ train_set = data(permutation(:,1:TRAIN_SIZE),:);
 test_set = data(permutation(:,TRAIN_SIZE+1:end),:);
 
 % bextract training
-train_attr = train_set(:,bextract);
+train_attr_pos = train_set(:,bextract_pos);
+train_attr_int = train_set(:,bextract_int);
 train_pos = train_set(:,2);
 train_int = train_set(:,3);
 
 % bextract testing
-test_attr = test_set(:,bextract);
+test_attr_pos = test_set(:,bextract_pos);
+test_attr_int = test_set(:,bextract_int);
 test_pos = test_set(:,2);
 test_int = test_set(:,3);
 std_pos = test_set(:,67);
@@ -29,8 +32,10 @@ std_int = test_set(:,66);
 
 %%% Un-comment one of the sections below
 %% Linear regression (arbitrary plane - not necessarily through origin)
-train_attr_mod = [ones(TRAIN_SIZE,1) train_attr];
-test_attr_mod = [ones(size(test_attr,1),1) test_attr];
+train_attr_mod_pos = [ones(TRAIN_SIZE,1) train_attr_pos];
+train_attr_mod_int = [ones(TRAIN_SIZE,1) train_attr_int];
+test_attr_mod_pos = [ones(size(test_attr_pos,1),1) test_attr_pos];
+test_attr_mod_int = [ones(size(test_attr_int,1),1) test_attr_int];
 
 %% Quadratic regression
 %train_attr_mod = [ones(TRAIN_SIZE,1) train_attr train_attr.^2];
@@ -39,11 +44,11 @@ test_attr_mod = [ones(size(test_attr,1),1) test_attr];
 
 %% Perform regression
 
-regress_pos = train_attr_mod\train_pos;
-regress_int = train_attr_mod\train_int;
+regress_pos = train_attr_mod_pos\train_pos;
+regress_int = train_attr_mod_int\train_int;
 
-results_pos = test_attr_mod * regress_pos;
-results_int = test_attr_mod * regress_int;
+results_pos = test_attr_mod_pos * regress_pos;
+results_int = test_attr_mod_int * regress_int;
 
 %% Results
 
